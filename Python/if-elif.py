c6e70d0393cbb9e25b9556551b7d5b18df26ac74
Liam Luo <ll24218@westlake.school.nz>
2026-06-18 09:22:28 +1200

    Moved HTML activites into the HTML folder

---
+++ Python/if-elif.py
@@ -0,0 +1,15 @@
+AGE = 16
+
+AGE = input("Enter your birth day ")
+AGE = input("Enter your birth month")
+AGE = input("Enter your birth year")
+
+AGE = input("Enter your target day")
+AGE = input("Enter your target month")
+AGE = input("Enter your target year")
+
+if AGE > 16:
+    print("No")
+
+if AGE <= 16:
+    print("Yes")
---
+++ Python/string-practice.py
@@ -0,0 +1,2 @@
+input("Enter a word. ")
+print()
